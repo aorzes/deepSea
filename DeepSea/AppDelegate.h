5cfe0c60c5358497c71e82e52750db1d8591b413
Anton Orzes <antonorzes@Antons-Mac-mini.local>
2017-01-22 19:17:39 +0100

    Initial Commit

---
+++ DeepSea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeepSea
+//
+//  Created by Anton Orzes on 22/01/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeepSea/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  DeepSea
+//
+//  Created by Anton Orzes on 22/01/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
